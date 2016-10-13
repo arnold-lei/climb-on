ec5d08c946e8f58490be18898e1e26ee376ce007
Nick West <mail@nickwe.st>
2016-10-13 14:44:48 -0400

    fixed components/index link to user page

--- imports/ui/components/index.jsx
+++ imports/ui/components/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Index = () =>
   <div>
     <h3>This is the root, "localhost:3000/".
-    <br/>Try this url: <a href="/Bernie?lat=33&lng=999">localhost:3000/Bernie?lat=33&lng=999</a>
+    <br/>Try this url: <a href="/user/Bernie?lat=33&lng=999">localhost:3000/user/Bernie?lat=33&lng=999</a>
     </h3>
   </div>
   ;
