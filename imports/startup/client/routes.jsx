96218a706a5fa6eb836b55110cdff60982e286bf
Arnold Lei <arnold.lei88@gmail.com>
2016-10-13 20:54:12 -0400

    fixed the /map route

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -4,11 +4,11 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import { App } from '../../ui/layouts/app.jsx';
 import { Index } from '../../ui/components/index.jsx';
-import { One } from '../../ui/pages/one.jsx';
+import { Map } from '../../ui/pages/map.jsx';
 import { Two } from '../../ui/pages/two.jsx';
 import { Marker } from '../../ui/pages/Marker';
 import { User } from '../../ui/pages/user.jsx';
-import { Map } from '../../ui/map/Map.jsx';
+import { SimpleMapPage } from '../../ui/map/Map.jsx';
 
 import { NotFound } from '../../ui/pages/not-found.jsx';
 
@@ -21,14 +21,14 @@ Meteor.startup(() => {
 
           <IndexRoute component={Index} />
 
-          <Route path="/one" component={One} />
+          {/* <Route path="/one" component={One} /> */}
           <Route path="/two" component={Two} />
 
           {/* /user/:name Routes, url sends User Name, Latitude and Longitude values to the screen*/}
           {/* Check out the "import { User } from '../../ui/pages/user.jsx' " statement on line 10 of this file^^^*/}
           <Route path="/user/:name" component={User} />
           <Route path="/marker" component={Marker} />
-          <Route path="/map" component={ Map } />
+          <Route path="/map" component={Map} />
 
         </Route>
 
--- imports/ui/map/my_great_places.jsx
+++ imports/ui/map/my_great_places.jsx
@@ -1,22 +1,55 @@
-
-import React, { Component } from 'react';
-
-
+//
+// import React, { Component } from 'react';
+//
+//
 import {greatPlaceStyle} from './my_great_place_styles';
+//
+// export default class MyGreatPlace extends Component {
+//   // static propTypes = {
+//   //   text: PropTypes.string
+//   // };
+//   //
+//   // static defaultProps = {};
+//   //
+//   // shouldComponentUpdate = shouldPureComponentUpdate;
+//
+//   render() {
+//     return (
+//        <div style={greatPlaceStyle}>
+//           {this.props.text}
+//        </div>
+//     );
+//   }
+// }
+import React, {PropTypes, Component} from 'react';
+
+
+// import {greatPlaceStyle, greatPlaceStyleHover} from './my_great_place_with_controllable_hover_styles.js';
 
 export default class MyGreatPlace extends Component {
-  // static propTypes = {
-  //   text: PropTypes.string
-  // };
-  //
-  // static defaultProps = {};
-  //
+
   // shouldComponentUpdate = shouldPureComponentUpdate;
 
+  constructor(props) {
+    super(props);
+
+      this.propTypes= {
+        // use hover from controllable
+        hover: PropTypes.bool,
+        text: PropTypes.string
+    },
+        this.defaultProps = {}
+  }
+
   render() {
+    const style = this.props.hover ? greatPlaceStyleHover : greatPlaceStyle;
+
     return (
-       <div style={greatPlaceStyle}>
-          {this.props.text}
+       <div className="hint hint--html hint--info hint--top" style={style}>
+          <div>{this.props.text}</div>
+          <div style={{width: 80}} className="hint__content">
+          Сlick me
+          </div>
        </div>
     );
   }
